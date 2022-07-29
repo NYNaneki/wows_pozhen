95c1bdbe57b3c5ba475d484c9f6288112122931f
SIYUDI <1q2w3esc@gmail.com>
2022-07-29 20:45:20 +0800

    写了每次的移动方向 化棍为点

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,64 @@ import javax.swing.*;
 import java.util.Scanner;
 
 //7*7棋盘
+//迭代 先做点状船算了;
 class warship {
     int[] loc=new int[2];       //0是x坐标 1是y坐标
-    Integer ishorizontal;   //是否竖着，与前进后退方向
+    int ishorizontal; //
+    int hp=3;
     void set(){
         loc[0]=3;
         loc[1]=3;
         ishorizontal=0;
         System.out.println("settled!");
     }
-    void move(){
-        if(loc[ishorizontal]+1==6){
-            loc[ishorizontal]--;
-            return;
-        }
-        if(loc[ishorizontal]-1==0){
-            loc[ishorizontal]++;
-            return;
-        }
+    void move(){            //机制：先确定方向再前后
         int num=(int) (Math.random()*2);
-        System.out.println(num);
+        int xy=(int)(Math.random()*2); //0 x    1 y
+        if(loc[xy]+1>6){
+            loc[xy]--;
+            num=0;
+            return;
+        }
+        if(loc[xy]-1<0){
+            loc[xy]++;
+            num=1;
+            return;
+        }
+
         switch (num){
             case 1:
-        loc[ishorizontal]++;
+        loc[xy]++;
         break;
             case 0:
-        loc[ishorizontal]--;
+        loc[xy]--;
         break;}
+        String a="null";
+        switch (xy) {
+            case 0:
+                if(num==0)
+                    a="X-";
+                else a="X+";
+                break;
+            case 1:
+                if(num==0)
+                    a="Y-";
+                else a="Y+";
+                break;
+        }
+        System.out.print(a+"\t");
         System.out.println("moved!");
     }
     void getloc(){
         System.out.println(loc[0]+" "+loc[1]);
     }
-    void turn(){
+    /*void turn(){
         if(ishorizontal==1)
             ishorizontal=0;
         if(ishorizontal==0)
             ishorizontal=1;
         System.out.println("turned!");
-    }
+    }*/
 
 }
 class map{
@@ -62,6 +81,17 @@ public class Main {
         //Map.draw();
     warship w=new warship();
     w.set();
-
+    while(w.hp>0){
+        int x= Integer.parseInt(scanner.next());
+        int y=Integer.parseInt(scanner.next());
+        int d=Math.abs(x-w.loc[0])+Math.abs(y-w.loc[1]);
+        if(d==0){
+            System.out.println("hit!");
+            w.hp--;
+        }
+        else System.out.println(d);
+        w.move();
+    }
+    System.out.println("you win win!");
     }
 }
